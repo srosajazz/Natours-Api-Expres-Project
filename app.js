5794cf98e86889a6526e8e7e03d216cb48efd654
Sergio Rosa <scarlosjazz@gmail.com>
2019-06-29 16:56:19 -0400

    Using  Morgan - 3rd-Party Middleware

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 
+//1)MIDDLEWARES
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -19,6 +23,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+//2) ROUTE HANDLERS
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -107,6 +112,7 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id',updateTour);
 // app.delete('/api/v1/tours/:id',deleteTour);
 
+//3) ROUTES
 app
   .route('/api/v1/tours')
   .get(getAllTours)
@@ -117,6 +123,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+//START SERVER
 const port = 3001;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
