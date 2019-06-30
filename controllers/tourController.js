d16adf687c18dd076b03d8400ab3662108b4d7fa
Sergio Rosa <scarlosjazz@gmail.com>
2019-06-30 19:01:51 -0400

    Chaining Multiple Middleware Functions

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,16 +6,26 @@ const tours = JSON.parse(
 
 exports.checkID = (req, res, next, val) => {
   console.log(`Tour id is: ${val}`);
+
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
-      message: 'Ivalid ID'
+      message: 'Invalid ID'
     });
   }
   next();
 };
 
-//2) ROUTE HANDLERS
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price'
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
 
