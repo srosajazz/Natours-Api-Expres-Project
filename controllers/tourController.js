d193bc09551df7ed232952c3a6047cbcc5e147cd
Sergio Rosa <scarlosjazz@gmail.com>
2019-06-30 15:12:39 -0400

    Implementing Param Middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,17 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Ivalid ID'
+    });
+  }
+  next();
+};
+
 //2) ROUTE HANDLERS
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const tourController = require('./../controllers/tourController.js');
 const router = express.Router();
 
+router.param('id', tourController.checkID);
+
 router
   .route('/')
   .get(tourController.getAllTours)
